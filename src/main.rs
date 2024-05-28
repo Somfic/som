575c4acf8219dff7d7bd41f17b7876aa01bafe6f
Lucas <lucas@somfic.dev>
2024-05-29 01:40:19 +0200

    ⚰️ Remove unused code
    semver: chore

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,8 @@
 use anyhow::*;
-use std::{env, io::BufRead};
 
+pub mod parser;
 pub mod scanner;
 
 fn main() -> Result<()> {
-    let active_directory = std::env::current_dir().unwrap();
-    let file_name = env::args().nth(1).unwrap();
-    let file_path = active_directory.join(file_name);
-
-    let file = std::fs::File::open(file_path).unwrap();
-    let reader = std::io::BufReader::new(file);
-    let content = reader
-        .lines()
-        .map(|line| line.unwrap())
-        .collect::<Vec<String>>()
-        .join("\n");
-
     Ok(())
 }
