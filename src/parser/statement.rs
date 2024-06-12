08e660984e3d4e6ea5188f117c9ae69f1e0531cf
Lucas <lucas@somfic.dev>
2024-06-12 03:42:32 +0200

    ✨ (parser): Add variable declarations
    semver: minor

--- src/parser/statement.rs
+++ src/parser/statement.rs
@@ -1,4 +1,6 @@
-use crate::scanner::lexeme::{Lexeme, Range, TokenType};
+use core::panic;
+
+use crate::scanner::lexeme::{Lexeme, Range, TokenType, TokenValue};
 
 use super::{
     expression, lookup::BindingPower, macros::expect_token, Diagnostic, Parser, Statement,
@@ -19,14 +21,39 @@ pub fn parse(parser: &Parser, cursor: usize) -> Result<(Statement, usize), Diagn
 
     let lexeme = lexeme.unwrap();
 
-    if let Lexeme::Invalid(_) = lexeme {
-        return Err(Diagnostic::error(lexeme.range(), "Invalid token"));
+    let token = match lexeme {
+        Lexeme::Valid(token) => token,
+        Lexeme::Invalid(_) => return Err(Diagnostic::error(lexeme.range(), "Invalid token")),
+    };
+
+    let statement_handler = parser.lookup.statement_lookup.get(&token.token_type);
+
+    match statement_handler {
+        Some(statement_handler) => statement_handler(parser, cursor),
+        None => parse_expression(parser, cursor),
     }
+}
 
-    let (expression, new_cursor) = expression::parse(parser, cursor, &BindingPower::None)?;
-
-    // Expect a semicolon
-    let (_, cursor) = expect_token!(parser, new_cursor, TokenType::Semicolon)?;
+pub fn parse_expression(parser: &Parser, cursor: usize) -> Result<(Statement, usize), Diagnostic> {
+    let (expression, cursor) = expression::parse(parser, cursor, &BindingPower::None)?;
+    let (_, cursor) = expect_token!(parser, cursor, TokenType::Semicolon)?;
 
     Ok((Statement::Expression(expression), cursor))
+}
+
+pub fn parse_declaration(parser: &Parser, cursor: usize) -> Result<(Statement, usize), Diagnostic> {
+    let (_, cursor) = expect_token!(parser, cursor, TokenType::Var)?;
+    let (identifier, cursor) = expect_token!(parser, cursor, TokenType::Identifier)?;
+    let identifier = match &identifier.value {
+        TokenValue::Identifier(identifier) => identifier,
+        _ => panic!("expect_token! should return a valid token and handle the error case"),
+    };
+    let (_, cursor) = expect_token!(parser, cursor, TokenType::Equal)?;
+    let (expression, cursor) = expression::parse(parser, cursor, &BindingPower::None)?;
+    let (_, cursor) = expect_token!(parser, cursor, TokenType::Semicolon)?;
+
+    Ok((
+        Statement::Declaration(identifier.clone(), expression),
+        cursor,
+    ))
 }
