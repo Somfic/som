d2629bffef9164a2ea210ed57d2e2ac72001ebf1
Lucas <lucas@somfic.dev>
2024-10-29 02:02:42 +0100

    🚧 WIP
    semver: chore

---
+++ src/parser/expression/primitive.rs
@@ -0,0 +1,34 @@
+use crate::{
+    lexer::{TokenKind, TokenValue},
+    parser::{
+        ast::{Expression, Primitive},
+        Parser,
+    },
+};
+use miette::Result;
+
+pub fn integer<'de>(parser: &mut Parser) -> Result<Expression<'de>> {
+    let token = parser
+        .lexer
+        .expect(TokenKind::Integer, "expected an integer")?;
+
+    let value = match token.value {
+        TokenValue::Integer(v) => v,
+        _ => unreachable!(),
+    };
+
+    Ok(Expression::Primitive(Primitive::Integer(value)))
+}
+
+pub fn decimal<'de>(parser: &mut Parser) -> Result<Expression<'de>> {
+    let token = parser
+        .lexer
+        .expect(TokenKind::Decimal, "expected a decimal")?;
+
+    let value = match token.value {
+        TokenValue::Decimal(v) => v,
+        _ => unreachable!(),
+    };
+
+    Ok(Expression::Primitive(Primitive::Decimal(value)))
+}
