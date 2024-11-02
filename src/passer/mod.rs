8a13341fdb6f8767909a3fb03375b163eefacfee
Lucas <lucas@somfic.dev>
2024-11-02 16:08:16 +0100

    ✨ Add primitive type checker
    semver: minor

--- src/passer/mod.rs
+++ src/passer/mod.rs
@@ -1,5 +1,7 @@
+use std::collections::HashSet;
+
 use crate::parser::ast::Symbol;
-use miette::{Report, Result};
+use miette::{Report, Result, SourceSpan};
 
 pub mod typing;
 
@@ -7,7 +9,17 @@ pub trait Passer {
     fn pass(ast: &Symbol<'_>) -> Result<PasserResult>;
 }
 
+#[derive(Default)]
 pub struct PasserResult {
     pub non_critical: Vec<Report>,
     pub critical: Vec<Report>,
+}
+
+impl PasserResult {
+    pub fn combine(mut self, other: PasserResult) -> Self {
+        self.non_critical.extend(other.non_critical);
+        self.critical.extend(other.critical);
+
+        self
+    }
 }
