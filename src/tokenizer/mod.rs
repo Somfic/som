8d89b24c7484baab0eb88e94957cf58086837317
Lucas <lucas@somfic.dev>
2025-02-12 20:10:14 +0100

    🚧 WIP
    semver: chore

---
+++ src/tokenizer/mod.rs
@@ -0,0 +1,267 @@
+use crate::prelude::*;
+mod token;
+use miette::{LabeledSpan, SourceSpan};
+pub use token::*;
+
+pub struct Tokenizer<'ast> {
+    source_code: &'ast str,
+    remainder: &'ast str,
+    byte_offset: usize,
+    peeked: Option<Result<Token<'ast>>>,
+}
+
+impl<'ast> Tokenizer<'ast> {
+    pub fn new(source_code: &'ast str) -> Tokenizer<'ast> {
+        Tokenizer {
+            source_code,
+            remainder: source_code,
+            byte_offset: 0,
+            peeked: None,
+        }
+    }
+
+    pub fn expect(
+        &mut self,
+        expected: TokenKind,
+        error_message: impl Into<String>,
+    ) -> Result<Token<'ast>> {
+        let error_message = error_message.into();
+
+        match self.next() {
+            // The token is what we expected
+            Some(Ok(token)) if expected == token.kind => Ok(token),
+
+            // The token is not what we expected
+            Some(Ok(token)) => Err(vec![miette::diagnostic! {
+                labels = vec![
+                    token.label(format!("expected {} here", expected))
+                ],
+                help = format!("expected {}, got {} instead", expected, token.kind),
+                "{error_message}"
+            }]),
+            // There was an error parsing the token
+            Some(Err(e)) => Err(e),
+            // There are no more tokens to parse
+            None => Err(vec![miette::diagnostic! {
+                labels = vec![
+                    LabeledSpan::at_offset(self.byte_offset - 1, format!("expected {} here", expected))
+                ],
+                help = format!("{} was expected, but no more code was found", expected),
+                "unexpected end of input",
+            }]),
+        }
+    }
+
+    pub fn peek(&mut self) -> Option<&Result<Token<'ast>>> {
+        if self.peeked.is_some() {
+            return self.peeked.as_ref();
+        }
+
+        self.peeked = self.next();
+        self.peeked.as_ref()
+    }
+
+    pub fn peek_expect(&mut self, expected: TokenKind) -> Option<&Result<Token<'ast>>> {
+        match self.peek() {
+            Some(Ok(token::Token { kind, .. })) => {
+                if *kind == expected {
+                    self.peeked.as_ref()
+                } else {
+                    None
+                }
+            }
+            _ => None,
+        }
+    }
+
+    fn parse_compound_operator(
+        &mut self,
+        single: TokenKind,
+        compound: TokenKind,
+        expected_char: char,
+    ) -> Result<(TokenKind, TokenValue<'ast>)> {
+        if let Some(c) = self.remainder.chars().next() {
+            if c == expected_char {
+                self.remainder = &self.remainder[c.len_utf8()..];
+                self.byte_offset += c.len_utf8();
+                Ok((compound, TokenValue::None))
+            } else {
+                Ok((single, TokenValue::None))
+            }
+        } else {
+            Ok((single, TokenValue::None))
+        }
+    }
+}
+
+impl<'ast> Iterator for Tokenizer<'ast> {
+    type Item = Result<Token<'ast>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next) = self.peeked.take() {
+            return Some(next);
+        }
+
+        let mut chars = self.remainder.chars();
+
+        let start_offset = self.byte_offset;
+
+        let c = chars.next()?;
+        self.remainder = chars.as_str();
+        self.byte_offset += c.len_utf8();
+
+        let kind: Result<(TokenKind, TokenValue<'ast>)> = match c {
+            '(' => Ok((TokenKind::ParenOpen, TokenValue::None)),
+            ')' => Ok((TokenKind::ParenClose, TokenValue::None)),
+            '{' => Ok((TokenKind::CurlyOpen, TokenValue::None)),
+            '}' => Ok((TokenKind::CurlyClose, TokenValue::None)),
+            '[' => Ok((TokenKind::SquareOpen, TokenValue::None)),
+            ']' => Ok((TokenKind::SquareClose, TokenValue::None)),
+            ';' => Ok((TokenKind::Semicolon, TokenValue::None)),
+            ',' => Ok((TokenKind::Comma, TokenValue::None)),
+            '.' => Ok((TokenKind::Dot, TokenValue::None)),
+            '@' => Ok((TokenKind::At, TokenValue::None)),
+            '#' => Ok((TokenKind::Hash, TokenValue::None)),
+            '$' => Ok((TokenKind::Dollar, TokenValue::None)),
+            '|' => Ok((TokenKind::Pipe, TokenValue::None)),
+            '^' => Ok((TokenKind::Caret, TokenValue::None)),
+            '~' => Ok((TokenKind::Tilde, TokenValue::None)),
+            '?' => Ok((TokenKind::Question, TokenValue::None)),
+            ':' => Ok((TokenKind::Colon, TokenValue::None)),
+            '-' => self.parse_compound_operator(TokenKind::Minus, TokenKind::Arrow, '>'),
+            '+' => Ok((TokenKind::Plus, TokenValue::None)),
+            '*' => Ok((TokenKind::Star, TokenValue::None)),
+            '/' => Ok((TokenKind::Slash, TokenValue::None)),
+            '%' => Ok((TokenKind::Percent, TokenValue::None)),
+            '=' => self.parse_compound_operator(TokenKind::Equal, TokenKind::Equality, '='),
+            '!' => self.parse_compound_operator(TokenKind::Not, TokenKind::Inequality, '='),
+            '<' => {
+                self.parse_compound_operator(TokenKind::LessThan, TokenKind::LessThanOrEqual, '=')
+            }
+            '>' => self.parse_compound_operator(
+                TokenKind::GreaterThan,
+                TokenKind::GreaterThanOrEqual,
+                '=',
+            ),
+            'a'..='z' | 'A'..='Z' | '_' => {
+                // Identifiers
+                let mut ident = String::new();
+                ident.push(c);
+                while let Some(c) = self.remainder.chars().next() {
+                    if c.is_alphanumeric() || c == '_' {
+                        ident.push(c);
+                        self.remainder = &self.remainder[c.len_utf8()..];
+                        self.byte_offset += c.len_utf8();
+                    } else {
+                        break;
+                    }
+                }
+
+                match ident.as_str() {
+                    "if" => Ok((TokenKind::If, TokenValue::None)),
+                    "else" => Ok((TokenKind::Else, TokenValue::None)),
+                    "fn" => Ok((TokenKind::Function, TokenValue::None)),
+                    "true" => Ok((TokenKind::Boolean, TokenValue::Boolean(true))),
+                    "false" => Ok((TokenKind::Boolean, TokenValue::Boolean(false))),
+                    "let" => Ok((TokenKind::Let, TokenValue::None)),
+                    "type" => Ok((TokenKind::Type, TokenValue::None)),
+                    "struct" => Ok((TokenKind::Struct, TokenValue::None)),
+                    "enum" => Ok((TokenKind::Enum, TokenValue::None)),
+                    "trait" => Ok((TokenKind::Trait, TokenValue::None)),
+                    "bool" => Ok((TokenKind::BooleanType, TokenValue::None)),
+                    "int" => Ok((TokenKind::IntegerType, TokenValue::None)),
+                    "dec" => Ok((TokenKind::DecimalType, TokenValue::None)),
+                    "str" => Ok((TokenKind::StringType, TokenValue::None)),
+                    "char" => Ok((TokenKind::CharacterType, TokenValue::None)),
+                    "return" => Ok((TokenKind::Return, TokenValue::None)),
+                    ident => Ok((
+                        TokenKind::Identifier,
+                        TokenValue::Identifier(ident.to_string().into()),
+                    )),
+                }
+            }
+            // Whole and decimal numbers
+            '0'..='9' => {
+                let mut number = String::new();
+                number.push(c);
+                while let Some(c) = self.remainder.chars().next() {
+                    if c.is_ascii_digit() || c == '.' {
+                        number.push(c);
+                        self.remainder = &self.remainder[c.len_utf8()..];
+                        self.byte_offset += c.len_utf8();
+                    } else {
+                        break;
+                    }
+                }
+
+                if let Ok(num) = number.parse::<i64>() {
+                    Ok((TokenKind::Integer, TokenValue::Integer(num)))
+                } else if let Ok(num) = number.parse::<f64>() {
+                    Ok((TokenKind::Decimal, TokenValue::Decimal(num)))
+                } else {
+                    Err(vec![miette::diagnostic! {
+                        labels = vec![
+                            LabeledSpan::at(self.byte_offset - number.len()..self.byte_offset, "this number")
+                        ],
+                        "invalid number"
+                    }])
+                }
+            }
+            '"' => {
+                let mut string = String::new();
+                while let Some(c) = self.remainder.chars().next() {
+                    if c == '"' {
+                        self.remainder = &self.remainder[c.len_utf8()..];
+                        self.byte_offset += c.len_utf8();
+                        break;
+                    } else {
+                        string.push(c);
+                        self.remainder = &self.remainder[c.len_utf8()..];
+                        self.byte_offset += c.len_utf8();
+                    }
+                }
+
+                Ok((TokenKind::String, TokenValue::String(string.into())))
+            }
+            '\'' => {
+                let c = self.remainder.chars().next()?;
+                self.remainder = &self.remainder[c.len_utf8()..];
+                self.byte_offset += c.len_utf8();
+
+                if self.remainder.chars().next()? == '\'' {
+                    self.remainder = &self.remainder[c.len_utf8()..];
+                    self.byte_offset += c.len_utf8();
+                    Ok((TokenKind::Character, TokenValue::Character(c)))
+                } else {
+                    Err(vec![miette::diagnostic! {
+                        labels = vec![
+                            LabeledSpan::at(self.byte_offset..self.byte_offset + c.len_utf8(), "this character")
+                        ],
+                        "expected closing single quote"
+                    }])
+                }
+            }
+            ' ' | '\r' | '\t' | '\n' => {
+                return self.next();
+            }
+            _ => Err(vec![miette::diagnostic! {
+                labels = vec![
+                    LabeledSpan::at(self.byte_offset - c.len_utf8()..self.byte_offset, "this character")
+                ],
+                "unexpected character '{c}' in input"
+            }]),
+        };
+
+        let byte_length = self
+            .byte_offset
+            .checked_sub(start_offset)
+            .expect("byte_offset should never be less than start_offset");
+
+        Some(kind.map(|(kind, value)| Token {
+            kind,
+            value,
+            span: SourceSpan::new(start_offset.into(), byte_length),
+            original: &self.source_code[start_offset..self.byte_offset],
+        }))
+    }
+}
---
+++ src/tokenizer/token.rs
@@ -0,0 +1,248 @@
+use std::{borrow::Cow, fmt::Display, hash::Hash};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Token<'ast> {
+    pub kind: TokenKind,
+    pub value: TokenValue<'ast>,
+    pub original: &'ast str,
+    pub span: miette::SourceSpan,
+}
+
+impl Token<'_> {
+    pub fn label(&self, text: impl Into<String>) -> miette::LabeledSpan {
+        miette::LabeledSpan::at(self.span, text.into())
+    }
+}
+
+impl Display for Token<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.value {
+            TokenValue::None => write!(f, "{}", self.kind),
+            value => write!(f, "{}: {}", self.kind, value),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenValue<'ast> {
+    None,
+    Boolean(bool),
+    Integer(i64),
+    Decimal(f64),
+    String(Cow<'ast, str>),
+    Character(char),
+    Identifier(Cow<'ast, str>),
+}
+
+impl Display for TokenValue<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TokenValue::None => write!(f, ""),
+            TokenValue::Boolean(value) => write!(f, "{}", value),
+            TokenValue::Integer(value) => write!(f, "{}", value),
+            TokenValue::Decimal(value) => write!(f, "{}", value),
+            TokenValue::String(value) => write!(f, "{}", value),
+            TokenValue::Character(value) => write!(f, "{}", value),
+            TokenValue::Identifier(value) => write!(f, "{}", value),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum TokenKind {
+    /// A token that should be ignored. This is used for whitespace, comments, etc.
+    Ignore,
+
+    /// The opening of an indentation level.
+    IndentationOpen,
+    /// The closing of an indentation level.
+    IndentationClose,
+
+    /// An opening parenthesis; `(`.
+    ParenOpen,
+    /// A closing parenthesis; `)`.
+    ParenClose,
+    /// An opening curly brace; `{`.
+    CurlyOpen,
+    /// A closing curly brace; `}`.
+    CurlyClose,
+    /// An opening square bracket; `[`.
+    SquareOpen,
+    /// A closing square bracket; `]`.
+    SquareClose,
+
+    /// A comma; `,`.
+    Comma,
+    /// A dot; `.`.
+    Dot,
+    /// A colon; `:`.
+    Colon,
+    /// A semicolon; `;`.
+    Semicolon,
+
+    /// A plus sign; `+`;
+    Plus,
+    /// A minus sign; `-`.
+    Minus,
+    /// A forward slash; `/`.
+    Slash,
+    /// An asterisk; `*`.
+    Star,
+    /// A percent sign; `%`.
+    Percent,
+
+    /// An equals sign; `=`.
+    Equal,
+    /// A negetion sign; `!`.
+    Not,
+    /// A less-than sign; `<`.
+    LessThan,
+    /// A greater-than sign; `>`.
+    GreaterThan,
+    /// A less-than-or-equal sign; `<=`.
+    LessThanOrEqual,
+    /// A greater-than-or-equal sign; `>=`.
+    GreaterThanOrEqual,
+    /// An equality sign; `==`.
+    Equality,
+    /// An inequality sign; `!=`.
+    Inequality,
+    /// An and sign; `&&`.
+    And,
+    /// An or sign; `||`.
+    Or,
+
+    /// An at sign; `@`.
+    At,
+    /// A hash sign; `#`.
+    Hash,
+    /// A dollar sign; `$`.
+    Dollar,
+    /// A tilde sign; `~`.
+    Tilde,
+    /// An arrow; `->`.
+    Arrow,
+    /// A question mark; `?`.
+    Question,
+    /// A pipe; `|`.
+    Pipe,
+    /// An caret; `^`.
+    Caret,
+
+    /// An if keyword; `if`.
+    If,
+    /// An else keyword; `else`.
+    Else,
+
+    /// A while keyword; `while`.
+    While,
+    /// A for keyword; `for`.
+    For,
+
+    /// A var keyword; `var`.
+    Let,
+    /// A type keyword; `type`.
+    Type,
+
+    /// A function keyword; `fn`.
+    Function,
+    /// A return keyword; `return`.
+    Return,
+
+    /// A boolean; `true`, `false`.
+    Boolean,
+    /// A number; `42`, `12`, `-7`.
+    Integer,
+    /// A decimal; `3.14`, `2.718`, `-1.0`.
+    Decimal,
+    /// A string; `"foo"`, `"bar"`, `"baz"`.
+    String,
+    /// A character; `'a'`, `'b'`, `'c'`.
+    Character,
+
+    /// An identifying name; `foo`, `bar`, `baz`.
+    Identifier,
+
+    /// A struct keyword; `struct`.
+    Struct,
+    /// A enum keyword; `enum`.
+    Enum,
+    /// A trait keyword; `trait`.
+    Trait,
+
+    /// The boolean type; `bool`.
+    BooleanType,
+    /// The integer type; `int`.
+    IntegerType,
+    /// The decimal type; `dec`.
+    DecimalType,
+    /// The string type; `str`.
+    StringType,
+    /// The character type; `char`.
+    CharacterType,
+}
+
+impl Display for TokenKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TokenKind::Ignore => write!(f, ""),
+            TokenKind::IndentationOpen => write!(f, "opening indentation level"),
+            TokenKind::IndentationClose => write!(f, "closing indentation level"),
+            TokenKind::ParenOpen => write!(f, "`(`"),
+            TokenKind::ParenClose => write!(f, "`)`"),
+            TokenKind::CurlyOpen => write!(f, "`{{`"),
+            TokenKind::CurlyClose => write!(f, "`}}`"),
+            TokenKind::SquareOpen => write!(f, "`[`"),
+            TokenKind::SquareClose => write!(f, "`]`"),
+            TokenKind::Comma => write!(f, "`,`"),
+            TokenKind::Dot => write!(f, "`.`"),
+            TokenKind::Colon => write!(f, "`:`"),
+            TokenKind::Semicolon => write!(f, "`;`"),
+            TokenKind::Plus => write!(f, "`+`"),
+            TokenKind::Minus => write!(f, "`-`"),
+            TokenKind::Slash => write!(f, "`/`"),
+            TokenKind::Star => write!(f, "`*`"),
+            TokenKind::Equal => write!(f, "`=`"),
+            TokenKind::Not => write!(f, "`!`"),
+            TokenKind::LessThan => write!(f, "`<`"),
+            TokenKind::GreaterThan => write!(f, "`>`"),
+            TokenKind::LessThanOrEqual => write!(f, "`<=`"),
+            TokenKind::GreaterThanOrEqual => write!(f, "`>=`"),
+            TokenKind::Equality => write!(f, "`==`"),
+            TokenKind::Inequality => write!(f, "`!=`"),
+            TokenKind::If => write!(f, "`if`"),
+            TokenKind::Else => write!(f, "`else`"),
+            TokenKind::While => write!(f, "`while`"),
+            TokenKind::For => write!(f, "`for`"),
+            TokenKind::Let => write!(f, "`let`"),
+            TokenKind::Type => write!(f, "`type`"),
+            TokenKind::Function => write!(f, "`fn`"),
+            TokenKind::Return => write!(f, "`return`"),
+            TokenKind::Boolean => write!(f, "a boolean value"),
+            TokenKind::Integer => write!(f, "an integer value"),
+            TokenKind::Decimal => write!(f, "a decimal value"),
+            TokenKind::String => write!(f, "a string value"),
+            TokenKind::Character => write!(f, "a character value"),
+            TokenKind::Identifier => write!(f, "an identifier"),
+            TokenKind::Struct => write!(f, "`struct`"),
+            TokenKind::Enum => write!(f, "`enum`"),
+            TokenKind::Percent => write!(f, "`%`"),
+            TokenKind::At => write!(f, "`@`"),
+            TokenKind::Hash => write!(f, "`#`"),
+            TokenKind::Dollar => write!(f, "`$`"),
+            TokenKind::Tilde => write!(f, "`~`"),
+            TokenKind::Arrow => write!(f, "`->`"),
+            TokenKind::Question => write!(f, "`?`"),
+            TokenKind::Pipe => write!(f, "`|`"),
+            TokenKind::Caret => write!(f, "`^`"),
+            TokenKind::And => write!(f, "`&&`"),
+            TokenKind::Or => write!(f, "`||`"),
+            TokenKind::Trait => write!(f, "`trait`"),
+            TokenKind::BooleanType => write!(f, "a boolean type"),
+            TokenKind::IntegerType => write!(f, "an integer type"),
+            TokenKind::DecimalType => write!(f, "a decimal type"),
+            TokenKind::StringType => write!(f, "a string type"),
+            TokenKind::CharacterType => write!(f, "a character type"),
+        }
+    }
+}
