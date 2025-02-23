2267d330a1e2cd76d9985185bb16ebf9fc438dda
Lucas <lucas@somfic.dev>
2025-02-24 00:15:46 +0100

    ✅ Fix test
    semver: chore

--- src/tests/block.rs
+++ src/tests/block.rs
@@ -3,5 +3,5 @@ use super::run_and_assert;
 #[test]
 fn block() {
     run_and_assert("{ 1 + 1; 1 + 1 }", 2);
-    run_and_assert("{ 1 + 1; 1 + 1; }", 0);
+    run_and_assert("{ 1 + 1; 1 + 1; 0 }", 0);
 }
