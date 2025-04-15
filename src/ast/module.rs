a0a2c1ff41d2e7fef4b02136205c881677edafc6
Lucas <lucas@somfic.dev>
2025-04-15 17:50:25 +0200

    ♻️ remove unused struct and function declarations
    semver: chore

--- src/ast/module.rs
+++ src/ast/module.rs
@@ -1,10 +1,9 @@
 use miette::SourceSpan;
 
 use super::Expression;
-use super::StructMember;
+use super::GenericFunctionDeclaration;
+use super::IntrinsicFunctionDeclaration;
 use super::TypedExpression;
-use super::Typing;
-use std::{borrow::Cow, collections::HashMap};
 
 pub type TypedModule<'ast> = GenericModule<'ast, TypedExpression<'ast>>;
 pub type Module<'ast> = GenericModule<'ast, Expression<'ast>>;
@@ -13,60 +12,4 @@ pub type Module<'ast> = GenericModule<'ast, Expression<'ast>>;
 pub struct GenericModule<'ast, Expression> {
     pub intrinsic_functions: Vec<IntrinsicFunctionDeclaration<'ast>>,
     pub functions: Vec<GenericFunctionDeclaration<'ast, Expression>>,
-}
-
-pub type TypedFunctionDeclaration<'ast> = GenericFunctionDeclaration<'ast, TypedExpression<'ast>>;
-pub type FunctionDeclaration<'ast> = GenericFunctionDeclaration<'ast, Expression<'ast>>;
-
-#[derive(Debug, Clone)]
-pub struct Parameter<'ast> {
-    pub name: Cow<'ast, str>,
-    pub span: miette::SourceSpan,
-    pub ty: Typing<'ast>,
-}
-
-impl Parameter<'_> {
-    pub fn label(&self, text: impl Into<String>) -> miette::LabeledSpan {
-        miette::LabeledSpan::at(self.span, text.into())
-    }
-
-    pub fn span(mut self, span: SourceSpan) -> Self {
-        self.span = span;
-        self
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct StructDeclaration<'ast> {
-    pub name: Cow<'ast, str>,
-    pub span: miette::SourceSpan,
-    pub members: Vec<StructMember<'ast>>,
-}
-
-#[derive(Debug, Clone)]
-pub struct GenericFunctionDeclaration<'ast, Expression> {
-    pub name: Cow<'ast, str>,
-    pub span: miette::SourceSpan,
-    pub parameters: Vec<Parameter<'ast>>,
-    pub body: Expression,
-    pub explicit_return_type: Option<Typing<'ast>>,
-}
-
-#[derive(Debug, Clone)]
-pub struct IntrinsicFunctionDeclaration<'ast> {
-    pub name: Cow<'ast, str>,
-    pub span: miette::SourceSpan,
-    pub parameters: Vec<Parameter<'ast>>,
-    pub return_type: Typing<'ast>,
-}
-
-impl TypedFunctionDeclaration<'_> {
-    pub fn label(&self, text: impl Into<String>) -> miette::LabeledSpan {
-        miette::LabeledSpan::at(self.span, text.into())
-    }
-
-    pub fn span(mut self, span: SourceSpan) -> Self {
-        self.span = span;
-        self
-    }
 }
--- src/ast/statement.rs
+++ src/ast/statement.rs
@@ -2,10 +2,7 @@ use std::borrow::Cow;
 
 use miette::SourceSpan;
 
-use super::{
-    Expression, GenericFunctionDeclaration, IntrinsicFunctionDeclaration, StructDeclaration,
-    TypedExpression, Typing,
-};
+use super::{Expression, TypedExpression, Typing};
 
 pub type TypedStatement<'ast> = GenericStatement<'ast, TypedExpression<'ast>>;
 pub type Statement<'ast> = GenericStatement<'ast, Expression<'ast>>;
@@ -25,8 +22,6 @@ pub enum StatementValue<'ast, Expression> {
     WhileLoop(Expression, Box<GenericStatement<'ast, Expression>>),
     Function(GenericFunctionDeclaration<'ast, Expression>),
     Intrinsic(IntrinsicFunctionDeclaration<'ast>),
-    Struct(StructDeclaration<'ast>),
-    Enum(EnumDeclaration<'ast>)
 }
 
 impl<'ast> GenericStatement<'ast, Expression<'ast>> {
@@ -49,6 +44,62 @@ pub struct StructMember<'ast> {
 }
 
 impl StructMember<'_> {
+    pub fn label(&self, text: impl Into<String>) -> miette::LabeledSpan {
+        miette::LabeledSpan::at(self.span, text.into())
+    }
+
+    pub fn span(mut self, span: SourceSpan) -> Self {
+        self.span = span;
+        self
+    }
+}
+
+pub type TypedFunctionDeclaration<'ast> = GenericFunctionDeclaration<'ast, TypedExpression<'ast>>;
+pub type FunctionDeclaration<'ast> = GenericFunctionDeclaration<'ast, Expression<'ast>>;
+
+#[derive(Debug, Clone)]
+pub struct Parameter<'ast> {
+    pub name: Cow<'ast, str>,
+    pub span: miette::SourceSpan,
+    pub ty: Typing<'ast>,
+}
+
+impl Parameter<'_> {
+    pub fn label(&self, text: impl Into<String>) -> miette::LabeledSpan {
+        miette::LabeledSpan::at(self.span, text.into())
+    }
+
+    pub fn span(mut self, span: SourceSpan) -> Self {
+        self.span = span;
+        self
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct StructDeclaration<'ast> {
+    pub name: Cow<'ast, str>,
+    pub span: miette::SourceSpan,
+    pub members: Vec<StructMember<'ast>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct GenericFunctionDeclaration<'ast, Expression> {
+    pub name: Cow<'ast, str>,
+    pub span: miette::SourceSpan,
+    pub parameters: Vec<Parameter<'ast>>,
+    pub body: Expression,
+    pub explicit_return_type: Option<Typing<'ast>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct IntrinsicFunctionDeclaration<'ast> {
+    pub name: Cow<'ast, str>,
+    pub span: miette::SourceSpan,
+    pub parameters: Vec<Parameter<'ast>>,
+    pub return_type: Typing<'ast>,
+}
+
+impl TypedFunctionDeclaration<'_> {
     pub fn label(&self, text: impl Into<String>) -> miette::LabeledSpan {
         miette::LabeledSpan::at(self.span, text.into())
     }
