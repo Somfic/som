d0d6c4c7ad2ef503d5863044abef4e611d291dfa
Lucas <lucas@somfic.dev>
2025-05-15 23:34:14 +0200

    🔥 start over
    semver: chore

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,7 @@
-pub use prelude::*;
+use lexer::Lexer;
 
-use clap::arg;
-use clap::command;
-use clap::Parser;
-// use highlighter::SomHighlighter;
-use std::path::PathBuf;
-
-mod ast;
-mod compiler;
-mod highlighter;
-mod parser;
+mod lexer;
 mod prelude;
-mod runner;
-#[cfg(test)]
-mod tests;
-mod tokenizer;
-mod typer;
-
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-pub struct SomArgs {
-    #[arg(short, long)]
-    pub source_path: PathBuf,
-}
 
 fn main() {
     miette::set_hook(Box::new(|_| {
@@ -31,20 +10,19 @@ fn main() {
                 .terminal_links(true)
                 .unicode(true)
                 .context_lines(2)
-                // .with_syntax_highlighting(SomHighlighter {})
                 .build(),
         )
     }))
     .unwrap();
 
-    let args = SomArgs::parse();
+    let mut lexer = Lexer::new("123");
 
-    let source = std::fs::read_to_string(args.source_path).unwrap_or_else(|_| {
-        eprintln!("Failed to read source file.");
-        std::process::exit(1);
-    });
-
-    let result = run(Box::leak(source.into_boxed_str()));
-
-    println!("{result}")
+    match lexer.next_token() {
+        Ok(token) => println!("Next token: {:?}", token),
+        Err(errors) => {
+            for error in errors {
+                eprintln!("{:?}", miette::miette!(error).with_source_code("123"));
+            }
+        }
+    }
 }
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,47 +1,26 @@
-use crate::{
-    compiler::Compiler, parser::Parser, runner::Runner, tokenizer::Tokenizer, typer::Typer,
-};
+pub use crate::lexer::Token;
+use miette::Diagnostic;
+pub use miette::SourceSpan;
+use thiserror::Error;
 
-pub use crate::ast::{combine_spans, CombineSpan};
+pub type Result<T> = std::result::Result<T, Vec<Error>>;
 
-pub type Result<T> = std::result::Result<T, Vec<miette::MietteDiagnostic>>;
-pub type ReportResult<T> = std::result::Result<T, Vec<miette::Report>>;
-
-pub fn run(source_code: &'static str) -> i64 {
-    let parser = Parser::new(Tokenizer::new(source_code));
-    let parsed = parser
-        .parse()
-        .map_err(|err| print_errors_with_codebase(err, source_code))
-        .expect("failed to parse codebase");
-
-    let typer = Typer::new(parsed);
-    let typed = typer
-        .type_check()
-        .map_err(|err| print_errors_with_codebase(err, source_code))
-        .expect("failed to type check codebase");
-
-    let compiler = Compiler::new(typed);
-    let compiled = compiler
-        .compile()
-        .map_err(print_errors)
-        .expect("failed to compile codebase");
-
-    let runner = Runner::new(compiled);
-
-    runner
-        .run()
-        .map_err(print_errors)
-        .expect("failed to run codebase")
+#[derive(Error, Debug, Diagnostic)]
+pub enum Error {
+    #[error(transparent)]
+    #[diagnostic(transparent)]
+    Lexer(LexerError),
 }
 
-fn print_errors_with_codebase(errors: Vec<miette::MietteDiagnostic>, source_code: &'static str) {
-    for error in errors {
-        eprintln!("{:?}", miette::miette!(error).with_source_code(source_code));
-    }
-}
+#[derive(Error, Debug, Diagnostic)]
+pub enum LexerError {
+    #[error("unexpected character")]
+    #[diagnostic()]
+    UnexpectedCharacter {
+        #[label("primary")]
+        token: Token,
 
-fn print_errors(errors: Vec<miette::Report>) {
-    for error in errors {
-        eprintln!("{error:?}");
-    }
+        #[help]
+        help: Option<String>,
+    },
 }
