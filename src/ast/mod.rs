88e659c203d63fcd6af570450c14c9c078bca92c
Lucas <lucas@somfic.dev>
2025-05-18 18:40:47 +0200

    🔥 remove `span_derive` crate and related files
    semver: chore

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,11 +1,5 @@
 pub use crate::prelude::*;
 
-pub struct TypedExpression {
-    pub value: ExpressionValue,
-    pub span: SourceSpan,
-    pub type_: Type,
-}
-
 pub type Statement = GenericStatement<Expression>;
 pub type TypedStatement = GenericStatement<TypedExpression>;
 pub struct GenericStatement<Expression> {
