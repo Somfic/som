22930287e072a912b528141c74b691453cd0babc
Lucas <lucas@somfic.dev>
2026-06-23 11:22:59 +0200

    update rust version

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
 
-        rustToolchain = pkgs.rust-bin.stable."1.90.0".default.override {
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
           extensions = [ "rust-src" "rust-analyzer" "clippy" ];
         };
       in {
